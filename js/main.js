b6b64a590c2448927f35ae77e4dfa4a4845513ba
Matheus Webler <matheus.webler@gmail.com>
2015-10-06 05:17:27 +0000

    fixed home panel

--- js/main.js
+++ js/main.js
@@ -10,117 +10,31 @@ $(document).ready(function() {
         $.get("editor.html").success(function(data) {
             console.log(data);
             bootbox.dialog({
-                /**
-                 * @required String|Element
-                 */
                 message: data,
-
-                /**
-                 * @optional String|Element
-                 * adds a header to the dialog and places this text in an h4
-                 */
-                title: "Custom title",
-
-                /**
-                 * @optional Function
-                 * allows the user to dismisss the dialog by hitting ESC, which
-                 * will invoke this function
-                 */
+                title: '<input type="text" id="text-editor-title" placeholder="Template Title">',
                 onEscape: function() {},
-
-                /**
-                 * @optional Boolean
-                 * @default: true
-                 * whether the dialog should be shown immediately
-                 */
                 show: true,
-
-                /**
-                 * @optional Boolean
-                 * @default: true
-                 * whether the dialog should be have a backdrop or not
-                 */
                 backdrop: true,
-
-                /**
-                 * @optional Boolean
-                 * @default: true
-                 * show a close button
-                 */
                 closeButton: true,
-
-                /**
-                 * @optional Boolean
-                 * @default: true
-                 * animate the dialog in and out (not supported in < IE 10)
-                 */
                 animate: true,
-
-                /**
-                 * @optional String
-                 * @default: null
-                 * an additional class to apply to the dialog wrapper
-                 */
-                className: "my-modal",
-
-                /**
-                 * @optional Object
-                 * @default: {}
-                 * any buttons shown in the dialog's footer
-                 */
                 buttons: {
-
-                    // For each key inside the buttons object...
-
-                    /**
-                     * @required Object|Function
-                     * 
-                     * this first usage will ignore the `success` key
-                     * provided and take all button options from the given object
-                     */
-                    success: {
-                        /**
-                         * @required String
-                         * this button's label
-                         */
-                        label: "Success!",
-
-                        /**
-                         * @optional String
-                         * an additional class to apply to the button
-                         */
-                        className: "btn-success",
-
-                        /**
-                         * @optional Function
-                         * the callback to invoke when this button is clicked
-                         */
+                    "Save": {
+                        className: "btn-success btn-block",
                         callback: function() {}
                     },
 
-                    /**
-                     * this usage demonstrates that if no label property is
-                     * supplied in the object, the key is used instead
-                     */
-                    "Danger!": {
-                        className: "btn-danger",
+                    "Cancel": {
+                        className: "btn-danger btn-block",
                         callback: function() {}
                     },
-
-                    /**
-                     * lastly, if the value supplied is a function, the options
-                     * are assumed to be the short form of label -> callback
-                     * this is the most condensed way of providing useful buttons
-                     * but doesn't allow for any configuration
-                     */
-                    "Another label": function() {}
                 }
             });
 
-            var fullEditor = new Quill('#full-editor', {
+            /* global Quill*/
+            var fullEditor = new Quill('#text-editor-input', {
                 modules: {
                     'toolbar': {
-                        container: '#full-toolbar'
+                        container: '#text-editor-toolbar'
                     },
                     'link-tooltip': true
                 },

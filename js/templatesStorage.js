76c3ccbd7bd26f504e02a7798f9674ec184e1a4e
Matheus Webler <matheus.webler@gmail.com>
2015-10-11 23:35:18 -0300

    working on unit tests

--- js/templatesStorage.js
+++ js/templatesStorage.js
@@ -8,60 +8,110 @@ text - template content
 */
 
 var templatesStorage = (function () {
-    var templates = 'templates';
+    var storage = 'development';
+    var storageTemplates = storage + '-templates';
+    var storageUid = storage + '-uid';
+    var storageOptions = storage + '-options';
+    
+    function setStorage(local){
+        storage = local;
+        storageTemplates = storage + '-templates';
+        storageUid = storage + '-uid';
+        options = storage + '-options';
+    }
 
+    /* Return the item object that match the ID */
     function getById(id, callback){
-        chrome.storage.sync.get(templates, function(items) {
+        chrome.storage.sync.get(storageTemplates, function(items) {
             if(chrome.runtime.lastError){
-                callback([]);
+                callback(undefined);
             }
             else{
-                var item = items[templates].filter(function( obj ) {
+                var item = items[storageTemplates].filter(function( obj ) {
                     return obj.id == id;
                 });
-                if(item.length == 1)
-                    item = item[0];
-                callback(item);
+                if(item.length != 0)
+                    callback(item[0]);
+                else
+                    callback(undefined);
             }
         });
     }
     
+    /* Return an array containing all the templates from storage */
     function getAll(callback){
-        chrome.storage.sync.get(templates, function(items) {
+        chrome.storage.sync.get(storageTemplates, function(items) {
             if(chrome.runtime.lastError){
                 callback([]);
             }
             else{
-                callback(items[templates]);
+                callback(items[storageTemplates]);
             }
         });
     }
 
-    function update(template){
-        chrome.storage.sync.get(templates, function(items) {
-            if(chrome.runtime.lastError){
-                return;
-            }
-            else{
-                var t = [];
-                t.push(template);
-                var x = {'templates': t};
-                
-                chrome.storage.sync.set(x);
-            }
-        });
+    /* Add a new template or update if there is already one with same ID */
+    function update(template, callback){
+        var update_template = function(){
+            chrome.storage.sync.get(storageTemplates, function(items) {
+                if(chrome.runtime.lastError){
+                    return;
+                }
+                else{
+                    var templates = items[storageTemplates];
+                    if(templates === undefined)
+                        templates = [];
+                        
+                    templates.push(template);
+                    var obj = {};
+                    obj[storageTemplates] = templates;
+                    
+                    chrome.storage.sync.set(obj, function(){
+                        if(callback)
+                            callback(template);
+                    });
+                }
+            });
+        };
+        
+        if(template.id === undefined){
+           getUid(function(uid){
+              template.id = uid;
+              update_template();
+           });
+        }
+        else{
+            update_template();
+        }
     }
     
+    /* Delete item object that match the ID */
     function deleteById(id){
         
     }
     
-    function deleteAll(){
-        
+    /* Delete all the templates from storage */
+    function deleteAll(callback){
+        chrome.storage.sync.remove(storageTemplates, callback);
     }
     
-    function generateUid(){
-        
+    /* Return an unique id to be used in a template */
+    function getUid(callback){
+        chrome.storage.sync.get(storageUid, function(items) {
+            var next_uid;
+            if(chrome.runtime.lastError){
+                next_uid = 1;
+            }
+            else{
+                var last_uid = items[storageUid];
+                if(last_uid === undefined)
+                    next_uid = 1;
+                else
+                    next_uid = last_uid + 1;
+                chrome.storage.sync.set({storageUid: next_uid});
+            }
+            callback(next_uid);
+        });
     }
  
   // Return an object exposed to the public
@@ -71,5 +121,7 @@ var templatesStorage = (function () {
       update:       update,
       deleteById:   deleteById,
       deleteAll:    deleteAll,
+      getUid:       getUid,
+      setStorage:   setStorage
   };
 })();
--- tests/tests.js
+++ tests/tests.js
@@ -1,17 +1,48 @@
+
 QUnit.test( "Create and get (plain-text)", function( assert ) {
+    templatesStorage.setStorage('test1');
+    
     var done = assert.async();
     
     var template = {
-        id: "0123456789", 
         title:"my first template", 
         type:"plain-text", 
         text:"Template content"};
 
-    templatesStorage.update(template);
+    var update_done = function(template){
+        templatesStorage.getById(template.id, function(get_template){
+            assert.deepEqual( get_template, template, "Read tempalte different from inserted template" );
+            done();
+        });
+    };
+
+    templatesStorage.update(template, update_done);
+});
+
+QUnit.test( "Create two and get all (plain-text)", function( assert ) {
+    templatesStorage.setStorage('test2');
     
-    templatesStorage.getById(template.id, function(get_template){
-        assert.deepEqual( get_template, template, "Read tempalte different from inserted template" );
-        done();
-    });
+    var done = assert.async();
     
+    var template = {
+        title:"my first template", 
+        type:"plain-text", 
+        text:"Template content"};
+
+    var do_test = function(){
+        templatesStorage.update(template, function(template){
+            templatesStorage.update(template, function(template){
+                templatesStorage.getAll(function(get_templates){
+                    assert.equal( get_templates.length, 2, "Insert and read all templates failed" );
+                    done();
+                });
+            });
+        });
+    };
+    
+    templatesStorage.deleteAll(do_test);
+});
+
+QUnit.done(function( details ) {
+    templatesStorage.deleteAll();
 });

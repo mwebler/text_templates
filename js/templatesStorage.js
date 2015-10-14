15d40d3b97933366e1e8d7f236535944b88b4e51
Matheus Webler <matheus.webler@gmail.com>
2015-10-14 01:53:05 -0300

    fixed storage component with promise pattern

--- js/templatesStorage.js
+++ js/templatesStorage.js
@@ -21,68 +21,93 @@ var templatesStorage = (function () {
     }
 
     /* Return the item object that match the ID */
-    function getById(id, callback){
+    function getById(id){
+        var deferred = $.Deferred();
+        
         chrome.storage.sync.get(storageTemplates, function(items) {
             if(chrome.runtime.lastError){
-                callback(undefined);
+                deferred.reject('failed to get template');
+                return;
             }
             else{
-                var item = items[storageTemplates].filter(function( obj ) {
+                var templates = items[storageTemplates];
+                if(templates === undefined){
+                    deferred.reject('failed to get template');
+                    return;
+                }
+                
+                var template = templates.filter(function( obj ) {
                     return obj.id == id;
                 });
-                if(item.length != 0)
-                    callback(item[0]);
+                if(template.length != 0)
+                    deferred.resolve(template[0]);
                 else
-                    callback(undefined);
+                    deferred.reject('failed to get template');
             }
         });
+        
+        return deferred.promise();
     }
     
     /* Return an array containing all the templates from storage */
-    function getAll(callback){
+    function getAll(){
+        var deferred = $.Deferred();
+        
         chrome.storage.sync.get(storageTemplates, function(items) {
             if(chrome.runtime.lastError){
-                callback([]);
+                deferred.reject('failed to get all templates');
             }
             else{
-                callback(items[storageTemplates]);
+                var templates = items[storageTemplates];
+                if(templates === undefined)
+                    deferred.resolve([]);
+                else
+                    deferred.resolve(templates);
             }
         });
+        
+        return deferred.promise();
     }
 
     /* Add a new template or update if there is already one with same ID */
-    function update(template, callback){
+    function update(template){
+        var deferred = $.Deferred();
+        
         var update_template = function(){
-            chrome.storage.sync.get(storageTemplates, function(items) {
-                if(chrome.runtime.lastError){
-                    return;
-                }
-                else{
-                    var templates = items[storageTemplates];
-                    if(templates === undefined)
-                        templates = [];
-                        
-                    templates.push(template);
-                    var obj = {};
-                    obj[storageTemplates] = templates;
-                    
-                    chrome.storage.sync.set(obj, function(){
-                        if(callback)
-                            callback(template);
-                    });
-                }
+            var getPromise = getAll();
+            
+            getPromise.done(function(templates){
+                templates.push(template);
+                var obj = {};
+                obj[storageTemplates] = templates;
+                
+                chrome.storage.sync.set(obj, function(){
+                    deferred.resolve(template);
+                });
+            });
+            
+            getPromise.fail(function(){
+                deferred.reject('failed to insert template into storage');
             });
         };
         
         if(template.id === undefined){
-           getUid(function(uid){
-              template.id = uid;
-              update_template();
-           });
+            var uidPromise = getUid();
+            
+            uidPromise.done(function(uid){
+                template.id = uid;
+                update_template();  
+            });
+            
+            uidPromise.fail(function(){
+                deferred.reject('failed getting uid for template');
+            });
         }
         else{
             update_template();
         }
+        
+        return deferred.promise();
     }
     
     /* Delete item object that match the ID */
@@ -91,12 +116,25 @@ var templatesStorage = (function () {
     }
     
     /* Delete all the templates from storage */
-    function deleteAll(callback){
-        chrome.storage.sync.remove(storageTemplates, callback);
+    function deleteAll(){
+        var deferred = $.Deferred();
+        
+        chrome.storage.sync.remove(storageTemplates, function(){
+            if(chrome.runtime.lastError){
+                deferred.reject('failed removing all templates from storage');
+            }
+            else{
+                deferred.resolve();
+            }
+        });
+        
+        return deferred.promise();
     }
     
     /* Return an unique id to be used in a template */
-    function getUid(callback){
+    function getUid(){
+        var deferred = $.Deferred();
+        
         chrome.storage.sync.get(storageUid, function(items) {
             var next_uid;
             if(chrome.runtime.lastError){
@@ -110,8 +148,10 @@ var templatesStorage = (function () {
                     next_uid = last_uid + 1;
                 chrome.storage.sync.set({storageUid: next_uid});
             }
-            callback(next_uid);
+            deferred.resolve(next_uid);
         });
+        
+        return deferred.promise();
     }
  
   // Return an object exposed to the public
--- tests/tests.js
+++ tests/tests.js
@@ -1,46 +1,79 @@
+function qunit_init(){
+    //set timeout for 3seconds to avoid waiting for assynchronous functions
+    QUnit.config.testTimeout = 3000; 
+}
+
+function storage_init(){
+    //We don't want to sync test data. Use local storage instead of sync
+    chrome.storage.sync = chrome.storage.local; 
+    // do not mess up with productive storage
+    templatesStorage.setStorage('test'); 
+}
+
+qunit_init();
+storage_init();
 
 QUnit.test( "Create and get (plain-text)", function( assert ) {
-    templatesStorage.setStorage('test1');
-    
+    assert.expect(1);
     var done = assert.async();
     
-    var template = {
-        title:"my first template", 
-        type:"plain-text", 
-        text:"Template content"};
-
-    var update_done = function(template){
-        templatesStorage.getById(template.id, function(get_template){
-            assert.deepEqual( get_template, template, "Read tempalte different from inserted template" );
-            done();
+    function do_test(){
+        var template = {
+            title:"test 1", 
+            type:"plain-text", 
+            text:"Template content"};
+    
+        var updatePromise = templatesStorage.update(template);
+        
+        $.when(updatePromise).done(function(template){
+            var getPromise = templatesStorage.getById(template.id);
+            getPromise.done(function(get_template){
+                assert.deepEqual( get_template, template, "Read template different from inserted template" );
+                done();
+            });
+            
+            getPromise.fail(function(msg){
+                assert.ok(false, msg);
+            });
         });
-    };
-
-    templatesStorage.update(template, update_done);
+        
+        updatePromise.fail(function(msg){
+            assert.ok(false, msg);
+        });
+    }
+    
+    templatesStorage.deleteAll().then(do_test);
 });
 
 QUnit.test( "Create two and get all (plain-text)", function( assert ) {
-    templatesStorage.setStorage('test2');
-    
+    assert.expect(1);
     var done = assert.async();
     
-    var template = {
-        title:"my first template", 
-        type:"plain-text", 
-        text:"Template content"};
-
-    var do_test = function(){
-        templatesStorage.update(template, function(template){
-            templatesStorage.update(template, function(template){
-                templatesStorage.getAll(function(get_templates){
-                    assert.equal( get_templates.length, 2, "Insert and read all templates failed" );
-                    done();
-                });
+    function do_test(){
+    
+        var template = {
+            title:"test 2", 
+            type:"plain-text", 
+            text:"Template content"};
+            
+        function add(){
+            return templatesStorage.update(template);
+        }
+            
+        add().then(add).then(function(template){
+            var getPromise = templatesStorage.getAll();
+            getPromise.done(function(get_templates){
+                assert.equal( get_templates.length, 2, "Insert and read all templates failed" );
+                done();
+            });
+            
+            getPromise.fail(function(msg){
+                assert.ok(false, msg);
             });
         });
-    };
+    }
     
-    templatesStorage.deleteAll(do_test);
+    templatesStorage.deleteAll().then(do_test);
 });
 
 QUnit.done(function( details ) {
